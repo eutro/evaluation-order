16f5a9eb6b94c3714fa4753557cb74e3aededcfc
eutro <git@eutro.dev>
2021-04-16 17:23:01 +0100

    I'm ready

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.773"]
                  [org.clojure/core.async "0.4.500"]
-                 [play-cljc "0.9.4"]]
+                 [reagent "0.10.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]]
   :plugins [[lein-figwheel "0.5.20"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
   :source-paths ["src"]
