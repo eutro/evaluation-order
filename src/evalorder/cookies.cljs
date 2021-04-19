100f945ba7a36bcb511987955d29b872e66025ce
eutro <git@eutro.dev>
2021-04-19 22:25:01 +0100

    Change how the story is laid out

---
+++ src/evalorder/cookies.cljs
@@ -0,0 +1,18 @@
+(ns evalorder.cookies
+  (:require-macros [evalorder.macros :refer [! !js]])
+  (:require [clojure.string :as str]))
+
+(defn get-cookie [cookie]
+  (-> (! js/document :-cookie)
+      (str/split #"; ")
+      (->> (into {} (map #(str/split % #"=" 2))))
+      (get cookie)))
+
+(def one-year (* 60 60 24 356))
+
+(defn set-cookie!
+  ([cookie value]
+   (-> (! js/document :-cookie)
+       (set! (str/join [cookie "=" value ";"
+                        "max-age=" one-year
+                        "path=/"])))))
--- src/evalorder/lang/game.cljs
+++ src/evalorder/lang/game.cljs
@@ -2,7 +2,8 @@
   (:require [evalorder.lang.ast :as ast]
             [evalorder.lang.values :as values]
             [reagent.core :as reagent]
-            [cljs.spec.alpha :as s])
+            [cljs.spec.alpha :as s]
+            [evalorder.screen :as screen])
   (:require-macros [evalorder.macros :refer [! !js]]))
 
 (s/def ::expression
@@ -30,56 +31,22 @@
 (s/def ::definitions
   (s/coll-of ::definition, :into {}))
 
-(s/def ::description (s/every (s/nonconforming (s/or :s string? :n nil?))))
-
-(s/def ::post-description ::description)
-
-(s/def ::post-level ::description)
-
 (s/def ::target (s/coll-of ::expression, :kind set?, :into #{}))
 
-(s/def ::level (s/keys :req-un [::expression ::description ::target ::definitions]
-                       :opt-un [::post-description ::post-level]))
+(s/def ::level (s/keys :req-un [::expression ::target ::definitions]))
 
 (defn validate [level]
   (let [conformed (s/conform ::level level)]
     (when (s/invalid? conformed)
-      (throw (js/Error. (str "Invalid level:\n" (s/explain-str ::level level)))))
+      (throw (js/Error. (str "Invalid expr:\n" (s/explain-str ::level level)))))
     conformed))
-
-(defn button [name desc key on-click bar-content]
-  [:div {:className "button",
-         :onClick on-click,
-         :onMouseOver (fn [_]
-                        (reset!
-                          bar-content
-                          [:span {:className "tooltip"}
-                           desc (when key [:span " [" key "]"])]))
-         :onMouseOut (fn [] (reset! bar-content nil))}
-   name])
-
-(defn bar [content]
-  [:div {:class "bar"} @content])
-
-(defn describe [lines]
-  (when (seq lines)
-    [:div {:class "description"}
-     (for [[comment i] (map vector lines (range))]
-       ^{:key i}
-       [:div {:class "comment"}
-        (if comment
-          (str ";; " comment)
-          [:br])])]))
 
 (def reset-toggle (reagent/atom false))
 
-(defn level-node [{:keys [description
-                          post-description
-                          post-level
-                          expression
+(defn level-node [{:keys [expression
                           target
                           definitions]}
-                  finish-level]
+                  finish!]
   (let [selected (reagent/atom nil)
 
         select
@@ -99,41 +66,34 @@
             (swap! (or el @selected) #(with-meta (ast/evaluated %) (meta %))))
           (reset! evaluated true))]
     (fn []
-      [:<>
-       [:div {:class "code"
-              :onKeyDown
-              (fn [event]
-                (when-not (-> event (! :-ctrlKey))
-                  (let [key (-> event (! :-key))]
-                    (case key
-                      "ArrowUp" (some-> (:parent (meta @@selected)) select)
-                      "ArrowDown" (some-> (first (:children @@selected)) select)
-                      "ArrowLeft" (some-> (:lhs (meta @@selected)) select)
-                      "ArrowRight" (some-> (:rhs (meta @@selected)) select)
-                      "Enter" (evaluate)
-                      nil))))
-              :tabIndex -1
-              :ref (fn [el] (when el (! el :focus)))}
-        [describe description]
-        [:div {:class "expression"} (ast/render-child child)]
-        [describe post-description]
-        (when (and @evaluated (contains? target (ast/value @child)))
-          [:<>
-           [describe post-level]
-           [:div {:class "next-button"
-                  :onClick (fn [_] (finish-level))
-                  :onKeyDown (fn [event] (when (-> event (! :-key) (= "Enter")) (finish-level)))
-                  :tabIndex 0}
-            (ast/render (ast/->ListExpr nil [(atom (ast/->SymExpr nil 'next))]))]])]
+      (when (and @evaluated (contains? target (ast/value @child))) (finish!))
+      [:div {:class "code"
+             :onKeyDown
+             (fn [event]
+               (when-not (-> event (! :-ctrlKey))
+                 (let [key (-> event (! :-key))]
+                   (case key
+                     "ArrowUp" (some-> (:parent (meta @@selected)) select)
+                     "ArrowDown" (some-> (first (:children @@selected)) select)
+                     "ArrowLeft" (some-> (:lhs (meta @@selected)) select)
+                     "ArrowRight" (some-> (:rhs (meta @@selected)) select)
+                     "Enter" (evaluate)
+                     "r" (swap! reset-toggle not)
+                     nil))))
+             :tabIndex -1
+             :ref (fn [el] (when el (! el :focus)))}
+       [:div {:class "expression"} (ast/render-child child)]])))
 
-       (let [bar-content (reagent/atom nil)]
-         [:div {:class "buttons"}
-          [button "evaluate" "Evaluate the current expression" "Enter" (fn [_] (evaluate)) bar-content]
-          [button "reset" "Reset the level" nil (fn [_] (swap! reset-toggle not)) bar-content]
-          [bar bar-content]])])))
-
-(defn root [level finish-level]
+(defn root [level finish!]
   @reset-toggle
   ;; fully rerender it
   ^{:key (js/Date.now)}
-  [level-node level finish-level])
+  [level-node level finish!])
+
+(screen/add-reader!
+  'game/expression
+  (fn [expr]
+    (let [validated (validate expr)]
+      (reify screen/Element
+        (render [_ next!]
+          [root validated next!])))))
