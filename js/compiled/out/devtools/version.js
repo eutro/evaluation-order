98cdd355c2c300e19cdfbfd89a67b8baf12ce12b
Automation <eutro@users.noreply.github.com>
2021-04-22 22:09:28 +0000

    Build Pages

--- js/compiled/out/devtools/version.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.10.844 {:target :nodejs, :nodejs-rt false}
-goog.provide('devtools.version');
-goog.require('cljs.core');
-devtools.version.current_version = "1.0.0";
-devtools.version.get_current_version = (function devtools$version$get_current_version(){
-return devtools.version.current_version;
-});
-
-//# sourceMappingURL=version.js.map?rel=1619127700496
--- js/compiled/out/evalorder/util.cljs
+++
@@ -1,7 +0,0 @@
-(ns evalorder.util)
-
-(defn zip [& colls]
-  (apply map vector colls))
-
-(defn enumerate [coll]
-  (zip (range) coll))
