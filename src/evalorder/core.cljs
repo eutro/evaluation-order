b3f9582b3ecc418542d46bd2ee1d4bffd7f5bde3
eutro <benedek.szilvasy@gmail.com>
2021-04-23 14:04:14 +0100

    Support more levels

--- src/evalorder/core.cljs
+++ src/evalorder/core.cljs
@@ -10,18 +10,31 @@
   (:require-macros [evalorder.macros :refer [! !js]]))
 
 (def story (reagent/atom nil))
-(a/go (-> (!js :fetch "story.edn") (<p!) (! :text) (<p!) screen/read-screen (->> (reset! story))))
+(a/go (-> (!js :fetch "story.edn") (<p!) (! :text) (<p!) screen/read-story (->> (reset! story))))
 (def theme (reagent/atom (or (ck/get-cookie "EO_theme") "dark")))
 (add-watch theme :cookie (fn [_k _r _o n] (ck/set-cookie! "EO_theme" n)))
 (def theme->next {"dark" "", "" "dark"})
+(def progress (reagent/atom (let [cookie (some->> (ck/get-cookie "EO_progress")
+                                                  (!js :parseInt))]
+                              (if (integer? cookie) cookie 0))))
+(add-watch progress :cookie (fn [_k _r _o n] (ck/set-cookie! "EO_progress" (str n))))
+
+(defn level-select []
+  (let [selecting (reagent/atom false)]
+    (fn []
+      (if @selecting
+        `[:<>
+          ~@(for [level @story]
+              ^{:key name}
+              [menu/button
+               (:name level)
+               #(reset! menu/screen (partial screen/show (:scene level)))])]
+        [menu/button "Level Select" #(reset! selecting true)]))))
 
 (defn main-menu []
   [menu/menu
    [menu/logo]
-   `[~menu/button "Start"
-     ~@(if-some [s @story]
-         [(fn [] (reset! menu/screen (partial screen/show s)))]
-         [nil {:disabled true}])]
+   [level-select]
    [menu/button "Change Theme" (fn [] (swap! theme theme->next))]])
 
 (defn app []
