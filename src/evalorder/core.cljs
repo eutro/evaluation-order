667b155aa8c2f1790a4d098e411a471e84c71137
eutro <benedek.szilvasy@gmail.com>
2021-04-25 21:53:57 +0100

    Add level editor

--- src/evalorder/core.cljs
+++ src/evalorder/core.cljs
@@ -8,7 +8,8 @@
             [evalorder.util :as u]
             [evalorder.audio :as audio]
             [clojure.core.async :as a]
-            [cljs.core.async.interop :refer-macros [<p!]])
+            [cljs.core.async.interop :refer-macros [<p!]]
+            [clojure.pprint :as pp])
   (:require-macros [evalorder.macros :refer [! !js]]))
 
 (def failed-to-play (reagent/atom false))
@@ -18,11 +19,59 @@
        (catch js/Error e ;; Autoplay disallowed (more than likely)
          (reset! failed-to-play true))))
 
+(defn level-editor []
+  (let [v (reagent/atom
+           "{:name \"Name\"
+ :music :loop1
+ :scene
+ [\"This will be a comment\"
+  #game/expression {:expression (if true target), :target #{target}}
+  :continue
+  \"Wow\" #delay/ms 1000]}
+")]
+    (fn this []
+      [:<>
+       [:textarea {:class "editor"
+                   :spellCheck "false"
+                   :value @v
+                   :onChange (fn [event] (-> event (! :-target) (! :-value) (->> (reset! v))))}]
+       [:div {:class "editor-buttons"}
+        [menu/button
+         "Finish"
+         (fn []
+           (let [track @audio/current-track
+                 editor! (fn []
+                           (reset! menu/screen [this])
+                           (audio/play-track! track))]
+             (reset!
+              menu/screen
+              [screen/show
+               (doto (screen/read-level @v) prn)
+               editor!
+               (fn []
+                 (reset!
+                  menu/overlay
+                  [menu/menu
+                   [menu/logo "Paused"]
+                   [menu/options]
+                   [menu/button "Back to Editor"
+                    (fn []
+                      (editor!)
+                      (reset! menu/overlay nil))]
+                   [menu/back-button "Back to Game"]]))])))]
+        [menu/button
+         "Main Menu"
+         (fn [] (reset! menu/screen nil))]]])))
+
 (defn main-menu []
   [menu/menu
    [menu/logo "Evaluation Order"]
    [menu/level-select]
    [menu/options]
+   [menu/button
+    "Level Editor"
+    (fn []
+      (reset! menu/screen [level-editor]))]
    (when @failed-to-play
      [menu/button
       "Play Music"
